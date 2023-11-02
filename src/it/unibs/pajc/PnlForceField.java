c4159b8f547f556375bd05e1b7bc8c473e14a23b
Samuele Valperta <samuele.valperta@gmail.com>
2023-11-02 17:16:37 +0100

    "Integrate MouseMotionListener to PnlForceField"
    In the PnlForceField class, MouseMotionListener has been implemented. This was done by removing the 'stepNext' method and the existing timer logic. Now, an angle is calculated based on the mouse's position every time the mouse moves, allowing the panel to respond to mouse movement. This changes the user interaction, making the program more intuitive and responsive.

--- src/it/unibs/pajc/PnlForceField.java
+++ src/it/unibs/pajc/PnlForceField.java
@@ -2,19 +2,15 @@ package it.unibs.pajc;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Path2D;
 
-public class PnlForceField extends JPanel {
+public class PnlForceField extends JPanel implements MouseMotionListener {
     private double angle = 0;
-    public void stepNext(){
-        angle+=Math.PI/180;
-        repaint();
-    }
-
     public PnlForceField(){
-        javax.swing.Timer timer = new Timer(10, e -> stepNext());
-        timer.start();
+        this.addMouseMotionListener(this);
     }
     @Override
     protected void paintComponent(Graphics g) {
@@ -23,6 +19,8 @@ public class PnlForceField extends JPanel {
 
         int w = getWidth();
         int h = getHeight();
+
+        angle = Math.atan2(mousePosition.y-h/2, mousePosition.x-w/2);
 
         fillArrow(g2, w/2, h/2, 100, angle);
 
@@ -42,5 +40,17 @@ public class PnlForceField extends JPanel {
         t.rotate(angle);
 
         g2.fill(t.createTransformedShape(path));
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    private Point mousePosition = new Point(0,0);
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        mousePosition = e.getPoint();
+        repaint();
     }
 }
